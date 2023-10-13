b0374729021d50c4d0ed6b502c4e5b4e59c6a022
yauheni-se <yauheni.semianiuk@gmail.com>
2023-10-13 09:09:47 +0200

    disable setwd to run app for any user

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 Sys.setenv(lang = "en_US")
 options(shiny.sanitize.errors = FALSE, scipen = 999, dplyr.summarise.inform = FALSE)#warn = -1, 
 rm(list = ls())
-setwd('C:/Projects/wappapp/')
+#setwd('C:/Projects/wappapp/')
 files_to_exclude <- c('app.R', 'theme.css')
 
 for (i in list.files(recursive = TRUE)) {
